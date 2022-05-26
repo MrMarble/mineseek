ee7151008de423297ddf525925ee4d131736e5b8
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2022-05-26 15:03:25 +0200

    feat: add query and player methods

--- libs/database/database.go
+++ libs/database/database.go
@@ -20,7 +20,31 @@ type DB struct {
 }
 
 func (db *DB) InsertSLP(slp *minecraft.ServerListPing) error {
-	_, err := db.db.Exec("INSERT INTO servers VALUES ($1, $2, $3, $4, $5, $6);", slp.Host, slp.Port, slp.Version, slp.Favicon, slp.MOTD, slp.MaxPlayers)
+	_, err := db.db.Exec("INSERT INTO servers VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT UPDATE;", slp.Host, slp.Port, slp.Version, slp.Favicon, slp.MOTD, slp.MaxPlayers)
+	return err
+}
+
+func (db *DB) InsertQuery(query *minecraft.FullQuery) error {
+	_, err := db.db.Exec("INSERT INTO queries VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) ON CONFLICT UPDATE;", query.Host, query.Port, query.GameType, query.GameID, query.Version, query.Plugins, query.Map, query.MaxPlayers)
+	return err
+}
+
+func (db *DB) InsertPlayers(query *minecraft.FullQuery) error {
+	tx, err := db.db.Begin()
+	if err != nil {
+		return err
+	}
+	for _, player := range query.Players {
+		uuid, err := minecraft.GetUUID(player)
+		if err != nil {
+			return err
+		}
+		_, err = tx.Exec("INSERT INTO players VALUES ($1, $2) ON CONFLICT UPDATE;", uuid, player)
+		if err != nil {
+			return err
+		}
+	}
+	err = tx.Commit()
 	return err
 }
 
